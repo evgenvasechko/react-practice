46ccef7a8a2b0b56f39388a1501f15aaa1a68e22
Nikita Osotskiy <osotskiy@gmail.com>
2024-02-27 23:09:00 +0300

    Update thunk version

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,7 +2,7 @@ import {combineReducers, legacy_createStore as createStore} from "redux";
 import dialogsReducer from "../reducers/dialogs-reducer";
 import profileReducer from "../reducers/profile-reducer";
 import usersReducer from "../reducers/users-reducer";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 import { applyMiddleware } from 'redux';
 
 let reducers = combineReducers({
